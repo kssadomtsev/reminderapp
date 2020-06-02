23a9e523e0d7a4de9a60753352f221e023a69046
kssadomtsev <sadomcev.k@gmail.com>
2020-06-02 23:05:07 +0300

    Initial commit

---
+++ src/main/java/ru/oneonyx/reminderapp/Application.java
@@ -0,0 +1,13 @@
+package ru.oneonyx.reminderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
